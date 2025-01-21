895fbb1b668b34f8803d02f6737a63d3f7e3d814
coolcoder <email@email.com>
2025-01-21 00:47:37 -0500

    precommit

--- backend/alembic/versions/1a4f9e7c8707_add_data.py
+++ backend/alembic/versions/1a4f9e7c8707_add_data.py
@@ -7,13 +7,14 @@ Create Date: 2025-01-20 19:28:20.165138
 """
 from typing import Sequence, Union
 
+from sqlalchemy import MetaData, Table
+
 from alembic import op
 from powervision_models.powervision_models import Accounts, Base
-from sqlalchemy import Table, MetaData
 
 # revision identifiers, used by Alembic.
-revision: str = '1a4f9e7c8707'
-down_revision: Union[str, None] = 'dbe7a7d69816'
+revision: str = "1a4f9e7c8707"
+down_revision: Union[str, None] = "dbe7a7d69816"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -21,11 +22,8 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     # get metadata from current connection
 
-
-
     # define table representation
     op.execute("INSERT into accounts(name, password) VALUES('admin', 'admin')")
-
 
 
 def downgrade() -> None:
